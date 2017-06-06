9b9c24906687d885f5037e46763c57b21f5caa96
yashpatel5400 <ypatel@princeton.edu>
2017-06-06 17:44:33 -0400

    Added pages for testing creation/join

--- bopify/bopify.py
+++ bopify/bopify.py
@@ -1,10 +1,33 @@
+"""
+__name__		= bopify.py
+__author__	  = Cheetrios
+__description__ = Main application file for deployment
+"""
+
+import sqlite3
+
 from flask import Flask, render_template
+from flask import g
 
 app = Flask(__name__)
+DATABASE = 'db/sessions.db'
+
+def get_db():
+	db = getattr(g, '_database', None)
+	if db is None:
+		print(DATABASE)
+		g._database = sqlite3.connect(DATABASE)
+		db = g._database
+	return db
 
 @app.route("/")
-def hello():
-    return render_template("index.html")
+def login():
+	return render_template("index.html")
+
+@app.route("/session/")
+def session():
+	cur = get_db().cursor()
+	return render_template("session.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+	app.run(host='0.0.0.0', debug=True)
---
+++ bopify/db/README.md
@@ -0,0 +1,10 @@
+Database(s) are maintained as SQLite for clean implementation. The main
+ones are organized as follows:
+
+- Sessions (sessions.db)
+
+# Sessions
+The sessions DB contains all the sessions that have been created along with
+all the associated users that have listened to that session. It also tracks
+the associated master node user and meta data regarding the session, i.e.
+type of music typically played
